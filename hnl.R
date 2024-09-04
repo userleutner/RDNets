9063182da6a4878fafd072ffe64aa6198fed691a
leutnes97 <leutnes97@zedat.fu-berlin.de>
2024-09-04 20:38:27 +0200

    removed $Coop structure and changed reinvest strategy

--- hnl.R
+++ hnl.R
@@ -2,31 +2,13 @@ hnl = new.env()
 hnl$new <- function (n, token = rep(5, n)) {
   self = hnl
   self$token = token
-  self$Coop = matrix(0, nrow = n, ncol = n)
-}
+  }
 
-# replaced nested loops with vectorized operations
-hnl$coop <- function () {
-  self = hnl
-  n = nrow(self$Coop)
-  
-  lower_triangle <- lower.tri(self$Coop)
-  unpaired <- which(self$Coop == 0 & lower_triangle, arr.ind = TRUE)
-  
-  # determine chance for each unpaired relationship
-  token_pairs <- pmin(self$token[unpaired[, 1]], self$token[unpaired[, 2]])
-  pair_chances <- ifelse(token_pairs > 13, 0.1, ifelse(token_pairs > 3, 0.2 * ((token_pairs - 3) / 10), 0))
-  
-  # pair or unpair based on calculated chances
-  to_pair <- runif(length(pair_chances)) < pair_chances
-  self$Coop[unpaired[to_pair, , drop = FALSE]] <- 1
-  
-  # unpair logic for existing pairs
-  paired <- which(self$Coop == 1 & lower_triangle, arr.ind = TRUE)
-  to_unpair <- runif(nrow(paired)) < 0.2
-  self$Coop[paired[to_unpair, , drop = FALSE]] <- 0
-}
 
+# --------  Tax strategy -------#
+# after equal redistribution we carry the remainder to the next iteration, instead of ranomly redistributing the rest
+
+hnl$carried_forward_remainder <- 0
 
 hnl$tax_reinvest <- function(tax_rate) {
   self = hnl
@@ -41,35 +23,28 @@ hnl$tax_reinvest <- function(tax_rate) {
       total_tax = total_tax + tax_amount
     }
   }
+# add carried-forward remainder to the total tax
+  total_tax = total_tax + self$carried_forward_remainder
   
-  # strategy for the remainder, if total_tax has rest after division
-  if (total_tax > 0) {
-    redistribution = floor(total_tax / n)
-    remainder = total_tax %% n
-    
-    for (k in 1:n) {
-      self$token[k] = self$token[k] + redistribution
-    }
-   
-    # distribute remainder randomly 
-    if (remainder > 0) {
-      extra_tokens = sample(1:n, remainder)
-      for (k in extra_tokens) {
-        self$token[k] = self$token[k] + 1
-      }
-    }
+  # Redistribute equally
+  redistribution = floor(total_tax / n)
+  remainder = total_tax %% n
+  
+  for (k in 1:n) {
+    self$token[k] = self$token[k] + redistribution
   }
+   
+  # update carried-forward remainder for the next round
+  self$carried_forward_remainder = remainder
 }
 
-
-hnl$iter <- function (games=10,model="winlo",region=9,progress=TRUE,coop=FALSE, tax_rate=0.1) {
+# -------- Game Iteration ------- #
+hnl$iter <- function (games=10,model="winlo",region=9,progress=TRUE, tax_rate=0.1) {
     self = hnl
     n=length(self$token)
     idx=c(-region:-1,1:region)
     A = matrix(NA,nrow=n,ncol=n)
-    if (coop) {
-        self$coop()
-    }
+    
     for (g in 1:games) {
         start=sample(1:n,1)
         if (start==1) {
@@ -93,16 +68,7 @@ hnl$iter <- function (games=10,model="winlo",region=9,progress=TRUE,coop=FALSE, 
             }
             toki=self$token[i]
             tokj=self$token[j]
-            if (coop & self$Coop[i,j] != 1) {
-                if (sum(self$Coop[i,])>0) {
-                    idx=which(self$Coop[i,]==1)
-                    toki=toki+self$token[idx]
-                }
-                if (sum(self$Coop[j,])>0) {
-                    idx=which(self$Coop[j,]==1)
-                    tokj=tokj+self$token[idx]
-                }
-            }
+ 
             spoints=1
             if (progress) {
                 if (min(c(toki,tokj))>5) {
@@ -159,22 +125,11 @@ hnl$iter <- function (games=10,model="winlo",region=9,progress=TRUE,coop=FALSE, 
                         A[j,i] = -1
                         self$token[i]=self$token[i]+spoints
                         self$token[j]=self$token[j]-spoints
-                        if (self$token[j]<0) {
-                            idx=which(self$Coop[j,]==1)
-                            self$token[idx]=self$token[idx]+self$token[j]
-                            self$token[j]=0
-                        }
-
+                       
                     } else {
                         A[i,j] = -1
                         A[j,i] =  1
                         self$token[i]=self$token[i]-spoints
-                        if (self$token[i]<0) {
-                            idx=which(self$Coop[i,]==1)
-                            self$token[idx]=self$token[idx]+self$token[i]
-                            self$token[i]=0
-                        }
-
                         self$token[j]=self$token[j]+spoints
                     }
                 }
